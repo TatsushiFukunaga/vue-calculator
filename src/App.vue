21a91164ff8f7f8944d7e20a93f2398589983156
TatsushiFukunaga <tatsushi1789@gmail.com>
2021-12-14 17:46:38 +0900

    added History component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <div id="app">
     <Calculator />
+    <History :items="items" />
   </div>
 </template>
 
 <script>
 import Calculator from "./components/Calculator.vue";
+import History from "./components/History.vue";
 
 export default {
   name: "App",
   components: {
     Calculator,
+    History,
+  },
+  data() {
+    return {
+      items: ["123", "456", "789"],
+    };
   },
 };
 </script>
---
+++ src/components/History.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="history">
+    <div v-for="item in items" :key="item">
+      <div>{{ item }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["items"],
+};
+</script>
+
+<style scoped>
+.history {
+  padding-top: 60px;
+  font-size: 30px;
+}
+</style>
